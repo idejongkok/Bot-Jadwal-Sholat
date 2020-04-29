cf02a22abc02eb25694a71b916e2b8675da8b64b
uno <61499460+idejongkok@users.noreply.github.com>
2020-04-29 12:46:54 +0700

    Update Main.py
    update--

--- Main.py
+++ Main.py
@@ -10,50 +10,51 @@ page = requests.get(URL)
 soup = BeautifulSoup(page.text,'html.parser')
 #print(soup.prettify()) # <-- dipake buat cek aja
 bot = telepot.Bot('1267590625:AAHJV6vU6Q5KxMBs6CAYcehsshNp1s56h9I')
-now = datetime.datetime.now()
-jam = now.strftime("%H:%M")
 
 TabelWaktu = soup.find('tr', {'class':'table_highlight'})
-#Waktu = TabelWaktu.find_all('td')
 Shubuh = TabelWaktu.find_all('td')[1].get_text()
 Dzuhur = TabelWaktu.find_all('td')[2].get_text()
 Ashar = TabelWaktu.find_all('td')[3].get_text()
 Maghrib = TabelWaktu.find_all('td')[4].get_text()
 Isya = TabelWaktu.find_all('td')[5].get_text()
-okeh = "13:20"
 
 response = bot.getUpdates()
 
+   
+pprint(response)
+print ('Memulai..')
+print(Shubuh)
 
-def action(msg):
+while True:
 
+    TabelWaktu = soup.find('tr', {'class':'table_highlight'})
+    Shubuh = TabelWaktu.find_all('td')[1].get_text()
+    Dzuhur = TabelWaktu.find_all('td')[2].get_text()
+    Ashar = TabelWaktu.find_all('td')[3].get_text()
+    Maghrib = TabelWaktu.find_all('td')[4].get_text()
+    Isya = TabelWaktu.find_all('td')[5].get_text()
     now = datetime.datetime.now()
     jam = now.strftime("%H:%M")
-    chat_id = msg['chat']['id']
-    command = msg['text']
-    print ('Received: %s' % command)
-    if command == '/shubuh':
-        bot.sendMessage (chat_id, str("Waktu Shubuh pukul " + Shubuh))
-    elif command == '/dzuhur':
-        bot.sendMessage (chat_id, str("Waktu Dzuhur pukul " + Dzuhur))
-    elif command == '/ashar':
-        bot.sendMessage (chat_id, str("Waktu Ashar pukul " + Ashar))
-    elif command == '/maghrib':
-        bot.sendMessage (chat_id, str("Waktu Maghrib pukul " + Maghrib))
-    elif command == '/isya':
-        bot.sendMessage (chat_id, str("Waktu Isya pukul " + Isya))
-    elif command == '/jam':
-        bot.sendMessage (chat_id, str(" jam sekarang" + str (jam)))
-    elif jam == okeh:
-        bot.sendMessage (chat_id, str(" berhasil"))
+    #okeh = "10:20"
+    #print(jam)
 
-   
-#print (bot.getMe())
-pprint(response)
-MessageLoop(bot, action).run_as_thread()
-print ('Up and Running....')
+    if jam == Shubuh :
+        print("Shubuh")
+        bot.sendMessage ('@JadwalSholatJakarta', str(" Waktunya Sholat Shubuh oi!"))
+    elif jam == Dzuhur :
+        print("Dzuhur")
+        bot.sendMessage ('@JadwalSholatJakarta', str(" Waktunya Sholat Dzuhur oi!"))
+    elif jam == Ashar :
+        print("Ashar")
+        bot.sendMessage ('@JadwalSholatJakarta', str(" Waktunya Sholat Ashar oi!"))
+    elif jam == Maghrib :
+        print("Maghrib")
+        bot.sendMessage ('@JadwalSholatJakarta', str(" Waktunya Sholat Maghrib oi!"))
+        bot.sendMessage ('@JadwalSholatJakarta', str(" Selamat Berbuka Puasa!"))
+    elif jam == Isya :
+        print("Isya")
+        bot.sendMessage ('@JadwalSholatJakarta', str(" Waktunya Sholat Isya oi!"))
 
-while 2:
-    time.sleep(10)
+    time.sleep(60)
 
 
